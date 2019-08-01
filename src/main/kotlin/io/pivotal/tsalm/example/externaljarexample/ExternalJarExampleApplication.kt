e20cb8d2980a2f76c72ae8cf32014d173abb78a7
Timo Salm <tsalm@Timos-MBP.fritz.box>
2019-08-01 12:40:43 +0200

    Add log statments to indicate whether the external jar is missing or not

--- src/main/kotlin/io/pivotal/tsalm/example/externaljarexample/ExternalJarExampleApplication.kt
+++ src/main/kotlin/io/pivotal/tsalm/example/externaljarexample/ExternalJarExampleApplication.kt
@@ -1,11 +1,30 @@
 package io.pivotal.tsalm.example.externaljarexample
 
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.sql.Driver
+
 
 @SpringBootApplication
-class ExternalJarExampleApplication
+class ExternalJarExampleApplication : CommandLineRunner {
+
+    private val log = LoggerFactory.getLogger(ExternalJarExampleApplication::class.java)
+
+    override fun run(vararg args: String?) {
+        log.info("Try to use external sql driver ...")
+        try {
+            val driver = Class.forName(org.h2.Driver::class.qualifiedName).getConstructor().newInstance() as Driver
+            log.info("-------------> Found driver with version: " + driver.majorVersion + "." + driver.minorVersion)
+        } catch (e: NoClassDefFoundError) {
+            log.info("No driver found!!!")
+        }
+
+        Thread.currentThread().join()
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<ExternalJarExampleApplication>(*args)
+    runApplication<ExternalJarExampleApplication>(*args)
 }
